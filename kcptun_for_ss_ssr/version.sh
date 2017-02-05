b6ff2ea63dabc0e2dd5ccd7f312834727be52771
Clang <clangcn@gmail.com>
2017-02-05 12:41:54 +0800

    Update version.sh

--- kcptun_for_ss_ssr/version.sh
+++ kcptun_for_ss_ssr/version.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 export LIBSODIUM_VER=1.0.11
+export LIBSODIUM_LINK="https://download.libsodium.org/libsodium/releases/libsodium-${LIBSODIUM_VER}.tar.gz"
 export MBEDTLS_VER=2.4.0
+export MBEDTLS_LINK="https://tls.mbed.org/download/mbedtls-${MBEDTLS_VER}-gpl.tgz"
 export SS_LIBEV_VER=3.0.0
+export SS_LIBEV_LINK="https://github.com/shadowsocks/shadowsocks-libev/releases/download/v${SS_LIBEV_VER}/shadowsocks-libev-${SS_LIBEV_VER}.tar.gz"
 export KCPTUN_VER=20170114
+export KCPTUN_AMD64_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-amd64-${KCPTUN_VER}.tar.gz"
+export KCPTUN_386_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-386-${KCPTUN_VER}.tar.gz"
